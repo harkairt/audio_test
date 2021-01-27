df7da01b8b55e3a49dc6d00780881e9be70ad8a8
harkairt <32367206+harkairt@users.noreply.github.com>
2021-01-27 14:02:55 +0100

    cp

--- lib/10_s_delay.dart
+++ lib/10_s_delay.dart
@@ -1,24 +1,19 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class With10SecondDelay extends StatelessWidget {
-  static runningFunction(String s) async {
-    int sum = await doTheCalc();
-    return "total sum is 55";
-  }
-
-  static Future<int> doTheCalc() {
+class With10SecondDelay extends HookWidget {
+  static runningFunction(String s) {
     int sum = 0;
     for (int i = 1; i <= 10; i++) {
       sleep(Duration(seconds: 1));
-      // await Future<void>.delayed(Duration(milliseconds: 1000));
       print(i);
       sum += i;
     }
-
-    return Future.value(sum);
+    return "total sum is $sum";
   }
 
   pauseFunction() async {
@@ -27,17 +22,30 @@ class With10SecondDelay extends StatelessWidget {
     print(runningFunction(''));
   }
 
+  computeMeaningOfWorld() {}
+
   @override
   Widget build(BuildContext context) {
-    pauseFunction();
+    final date = useState(DateTime.now());
+    useEffect(() {
+      Timer.periodic(Duration(seconds: 1), (timer) {
+        date.value = DateTime.now();
+      });
+      return;
+    }, []);
+
+    // pauseFunction();
     return Material(
       child: Center(
         child: Center(
-          child: Text(
-            "Tnx for waiting 10 seconds : check console for response",
-            style: TextStyle(
-              fontSize: 50,
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(date.value.toString()),
+              OutlineButton(onPressed: () {
+                runningFunction('');
+              }),
+            ],
           ),
         ),
       ),
